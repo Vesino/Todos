e77813797fb3aa6afedb62438d84f746c6966751
Luis Vargas <luisvargasr10@gmail.com>
2022-12-13 19:28:05 -0600

    Create Todos table, get Todos from API

--- client/src/components/Todos.jsx
+++ client/src/components/Todos.jsx
@@ -26,14 +26,32 @@ export default function Todos() {
     
   return (
     <>
+    <h1 className="mt-2">Todos</h1>
+  
     { error && todos.length == 0 && <h1>An error occurred </h1>}
-    {todos.length > 0 && <ul>
-        {todos.map(todo => (
-            <li key={todo?.todo}>
-                {todo.todo}, {todo?.description}
-            </li>
-        ))}
-        </ul>
+    {todos.length > 0 && 
+      <table className="table table-striped">
+          <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">Todo</th>
+            <th scope="col">Description</th>
+            <th scope="col">Created at</th>
+            <th scope="col">Is Done</th>
+          </tr>
+        </thead>
+        <tbody>
+            {todos.map(todo => (
+              <tr key={todo?.id}>
+              <th scope="row">{todo?.id}</th>
+              <td>{todo?.todo}</td>
+              <td>{todo?.description}</td>
+              <td>{todo?.created_at}</td>
+              <td>{todo?.is_done}</td>
+            </tr>
+            ))}
+        </tbody>
+      </table>
     }
     </>
   )

e2b7dabd73de195a8cb174ac9b6247eb8dd9c9d7
Luis Vargas <luisvargasr10@gmail.com>
2023-01-16 14:36:40 -0600

    Add User POST to routes

--- server/cmd/api/routes.go
+++ server/cmd/api/routes.go
@@ -13,11 +13,15 @@ func (app *application) routes() http.Handler {
 
 	router.MethodNotAllowed = http.HandlerFunc(app.methodNotAllowedResponse)
 
+	// Todos
 	router.HandlerFunc(http.MethodGet, "/v1/todos", app.listTodos)
 	router.HandlerFunc(http.MethodPost, "/v1/todos", app.createTodoHandler)
 	router.HandlerFunc(http.MethodGet, "/v1/todos/:id", app.showTodoHandler)
 	router.HandlerFunc(http.MethodPut, "/v1/todos/:id", app.updateTodoHandler)
 	router.HandlerFunc(http.MethodDelete, "/v1/todos/:id", app.deleteTodoHandler)
+
+	// Users
+	router.HandlerFunc(http.MethodPost, "/v1/users", app.registerUserHandler)
 
 	return app.enableCORS(router)
 }
---
+++ server/cmd/api/users.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"errors"
+	"net/http"
+
+	"github.com/Vesino/todos/internal/data"
+	"github.com/Vesino/todos/internal/validator"
+)
+
+func (app *application) registerUserHandler(w http.ResponseWriter, r *http.Request) {
+	// create anonymous struct to hold the expected data from the request body
+	var input struct {
+		Name string `json:"name"`
+		Email string `jason:"email"`
+		Password string `json:"password"`
+	}
+
+	// parse the request body into the anonymouse input
+	err := app.readJSON(w,r,&input)
+	if err != nil {
+		app.badRequestResponse(w,r,err)
+		return
+	}
+
+	user := &data.User{
+		Name: input.Name,
+		Email: input.Email,
+		Activated: false,
+	}
+
+	// Password.Set() method to generate and store the hashed and plaintext passwords.
+	err = user.Password.Set(input.Password)
+	if err != nil {
+		app.serverErrorResponse(w,r,err)	
+		return
+	}
+	v := validator.New()
+
+	// Validate the user struct and return the error mesaage to the client if any of the checks fails
+	if data.ValidateUser(v, user); !v.Valid() {
+		app.failedValidationResponse(w,r,v.Errors)
+		return
+	}
+
+	// Insert the user into the DB
+	err = app.models.Users.Insert(user)
+	if err != nil {
+		switch {
+		case errors.Is(err, data.ErrDuplicatedEmail):
+			v.AddError("email", "a user with this email address already exists")
+			app.failedValidationResponse(w,r,v.Errors)
+		default:
+			app.serverErrorResponse(w,r,err)
+		}
+		return
+	}
+
+	// write a JSON response containing the user data along with the 201 Created Status code
+	err = app.writeJSON(w, 201, envelope{"user": user}, nil)
+	if err != nil {
+		app.serverErrorResponse(w,r,err)
+	}
+
+}

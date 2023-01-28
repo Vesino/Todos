f36e3b978a7d35d4954dd70ec5800f5eb50dab19
Luis Vargas <luisvargasr10@gmail.com>
2023-01-27 22:44:15 -0600

    Add tokens table, logic to create tokens associated to a user_id, add TokenModel to our Models

--- server/internal/data/models.go
+++ server/internal/data/models.go
@@ -15,12 +15,14 @@ var (
 type Models struct {
 	Todos TodoModel
 	Users UserModel
+	Tokens TokenModel
 }
 
 func NewModels(db *sql.DB) Models {
 	return Models{
 		Todos: TodoModel{DB: db},
 		Users: UserModel{DB: db},
+		Tokens: TokenModel{DB: db},
 	}
 }
 

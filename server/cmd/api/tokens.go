853501dd430c9fe9fcbd3773388bcf25937c79e6
Luis Vargas <luisvargasr10@gmail.com>
2023-01-30 22:12:55 -0600

    Add the authentication endpoint to return an authentication token

--- server/cmd/api/tokens.go
+++ server/cmd/api/tokens.go
@@ -1,6 +1,13 @@
 package main
 
-import "net/http"
+import (
+	"errors"
+	"net/http"
+	"time"
+
+	"github.com/Vesino/todos/internal/data"
+	"github.com/Vesino/todos/internal/validator"
+)
 
 func (app *application) createAuthenticationTokenHandler(w http.ResponseWriter, r *http.Request) {
 	var input struct {
@@ -13,4 +20,53 @@ func (app *application) createAuthenticationTokenHandler(w http.ResponseWriter, 
 		return
 	}
 
+	v := validator.New()
+
+	data.ValidateEmail(v, input.Email)
+	data.ValidatePasswordPlaintext(v, input.Password)
+
+	if !v.Valid() {
+		app.failedValidationResponse(w, r, v.Errors)
+		return
+	}
+
+	user, err := app.models.Users.GetByEmail(input.Email)
+
+	if err != nil {
+		switch {
+		case errors.Is(err, data.ErrRecordNotFound):
+		        app.invalidCredentialsResponse(w, r)
+		default:
+		        app.serverErrorResponse(w, r, err)
+		}
+		return
+	}
+
+	// Check if the provided password matches the actual password for the user.
+	match, err := user.Password.Matches(input.Password)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+		return
+	}
+	// If the passwords don't match, then we call the app.invalidCredentialsResponse()
+	// helper again and return.
+	if !match {
+	        app.invalidCredentialsResponse(w, r)
+		return
+	}
+
+	// Otherwise, if the password is correct, we generate a new token with a 24-hour
+	// expiry time and the scope 'authentication'.
+	token, err := app.models.Tokens.New(user.ID, 24 * time.Hour, data.ScopeAuthentication)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+		return
+	}
+
+	// Encode the token to JSON and send it in the response along with a 201 Created
+	// status code.
+	err = app.writeJSON(w, http.StatusCreated, envelope{"token": token}, nil)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+	}
 }
